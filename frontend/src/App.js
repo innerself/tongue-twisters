88fec3192782fd5b6824d9d67a8235329325b170
Sergey Petrin <spetrin@gmail.com>
2024-03-17 14:49:20 +0300

    Basic twister with inc/dec buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,81 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+import styled, {createGlobalStyle} from "styled-components";
+import twisters from "./twisters.js";
+
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: #EFEBE9;
+    }
+`;
+
+const AppContainer = styled.div`
+    display: flex;
+    justify-items: center;
+    border: 2px solid blue;
+    width: 99vw;
+    height: 99vh;
+`;
+
+const TwisterBlock = styled.div`
+    text-align: center;
+    border: 1px solid black;
+    margin: auto;
+    width: 70%;
+`;
+
+
+const TwisterRow = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 200px;
+    align-items: center;
+`;
+
+
+const FontChangeButton = ({name, setFSize, sizeChange}) => {
+    return <button
+        onClick={() => setFSize((currSize) => currSize + sizeChange)}>
+        {name}
+    </button>
+}
+
+const TwisterText = ({twisterText, fSize}) => {
+    return <p style={{fontSize: fSize}}>{twisterText}</p>
+}
+
+const TwisterControls = ({setFSize}) => {
+    return (
+        <span>
+            <FontChangeButton name="Increase" setFSize={setFSize} sizeChange={+5}/>
+            &nbsp;
+            <FontChangeButton name="Decrease" setFSize={setFSize} sizeChange={-5}/>
+        </span>
+    )
+}
+
+const Twister = ({twisterText}) => {
+    const [fSize, setFSize] = useState(30)
+    return (
+        <TwisterRow>
+            <TwisterText twisterText={twisterText} fSize={fSize}/>
+            <TwisterControls setFSize={setFSize}/>
+        </TwisterRow>
+    )
+};
+
 
 function App() {
-    const [message, setMessage] = useState('');
-
-    useEffect(() => {
-        fetch('/api/twisters')
-            .then(response => response.json())  // Parse the response as JSON
-            .then(dataList => {
-                if (dataList && dataList.length > 0) {
-                    console.log('Returned data: ', dataList);
-                    setMessage(dataList[0].text || 'No twisters :(');
-                } else {
-                    console.error('Empty or invalid data received from the API');
-                    // Set a default message or handle the case when data is empty
-                    setMessage("Default Message");
-                }
-            })
-            .catch(error => console.error('Error:', error));
-    }, []);
-
     return (
-        <div className="App">
-            <h1>{message}</h1>
-        </div>
+        <>
+            <GlobalStyle/>
+            <AppContainer>
+                <TwisterBlock>
+                    {twisters.map(({id, text}) => (
+                        <Twister key={`twister-${id}`} twisterText={text}/>
+                    ))}
+                </TwisterBlock>
+            </AppContainer>
+        </>
     );
 }
 
